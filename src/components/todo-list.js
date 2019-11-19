d3427adf7a34b6345b4e29fa59273ec938f329c6
orangepuer <rkhpuer@gmail.com>
2019-11-19 22:53:15 +0300

    Add props to TodoListItem component

--- src/components/todo-list.js
+++ src/components/todo-list.js
@@ -4,8 +4,8 @@ import TodoListItem from "./todo-list-item";
 const TodoList = () => {
   return (
     <ul>
-      <li><TodoListItem /></li>
-      <li><TodoListItem /></li>
+      <li><TodoListItem label ="Drink coffee" /></li>
+      <li><TodoListItem label = "Build react app" important /></li>
     </ul>
   );
 };
