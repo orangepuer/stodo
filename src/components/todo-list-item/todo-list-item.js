2912074548885abecdae12ee7cc8b44cf3c91764
orangepuer <rkhpuer@gmail.com>
2019-11-22 22:48:14 +0300

    Change the TodoListItem and ItemStatusFilter components to a class

--- src/components/todo-list-item/todo-list-item.js
+++ src/components/todo-list-item/todo-list-item.js
@@ -1,19 +1,20 @@
-import React from 'react'
+import React, { Component } from 'react'
 import './todo-list-item.css'
 
-const TodoListItem = ({ label, important = false }) => {
-  const style = {
-    color: important ? 'tomato' : 'black',
-    fontWeight: important ? 'bold' : 'normal'
-  };
+export default class TodoListItem extends Component {
+  render() {
+    const { label, important = false } = this.props;
+    const style = {
+      color: important ? 'tomato' : 'black',
+      fontWeight: important ? 'bold' : 'normal'
+    };
 
-  return (
-    <span className="todo-list-item">
+    return (
+      <span className="todo-list-item">
       <span className="todo-list-item-label" style={ style }>{ label }</span>
       <button className="btn btn-outline-success btn-sm float-right" type="button"><i className="fa fa-exclamation" /></button>
       <button className="btn btn-outline-danger btn-sm float-right" type="button"><i className="fa fa-trash-o" /></button>
     </span>
-  );
-};
-
-export default TodoListItem;
+    );
+  }
+}
