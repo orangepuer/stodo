0e133760653f591d1be17eea22099eb8d35aa983
orangepuer <rkhpuer@gmail.com>
2019-11-23 21:00:57 +0300

    Add onLabelClick event handler

--- src/components/todo-list-item/todo-list-item.js
+++ src/components/todo-list-item/todo-list-item.js
@@ -2,6 +2,10 @@ import React, { Component } from 'react'
 import './todo-list-item.css'
 
 export default class TodoListItem extends Component {
+  onLabelClick = () => {
+    console.log(`Done: ${this.props.label}`);
+  };
+
   render() {
     const { label, important = false } = this.props;
     const style = {
@@ -11,10 +15,10 @@ export default class TodoListItem extends Component {
 
     return (
       <span className="todo-list-item">
-      <span className="todo-list-item-label" style={ style }>{ label }</span>
-      <button className="btn btn-outline-success btn-sm float-right" type="button"><i className="fa fa-exclamation" /></button>
-      <button className="btn btn-outline-danger btn-sm float-right" type="button"><i className="fa fa-trash-o" /></button>
-    </span>
+        <span className="todo-list-item-label" style={ style } onClick={ this.onLabelClick }>{ label }</span>
+        <button className="btn btn-outline-success btn-sm float-right" type="button"><i className="fa fa-exclamation" /></button>
+        <button className="btn btn-outline-danger btn-sm float-right" type="button"><i className="fa fa-trash-o" /></button>
+      </span>
     );
   }
 }
