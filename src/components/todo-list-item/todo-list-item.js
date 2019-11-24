ce66822390b6a1f3c1613c68527e00b4d4cd5c30
orangepuer <rkhpuer@gmail.com>
2019-11-24 17:51:20 +0300

    Add state to TodoListItem component and update onLabelClick event handler

--- src/components/todo-list-item/todo-list-item.js
+++ src/components/todo-list-item/todo-list-item.js
@@ -2,19 +2,33 @@ import React, { Component } from 'react'
 import './todo-list-item.css'
 
 export default class TodoListItem extends Component {
+  state = {
+    done: false
+  };
+
   onLabelClick = () => {
     console.log(`Done: ${this.props.label}`);
+    this.setState({
+      done: true
+    });
   };
 
   render() {
     const { label, important = false } = this.props;
+    const { done } = this.state;
+    let classNames = "todo-list-item"
+
+    if(done) {
+      classNames += " done"
+    }
+
     const style = {
       color: important ? 'tomato' : 'black',
       fontWeight: important ? 'bold' : 'normal'
     };
 
     return (
-      <span className="todo-list-item">
+      <span className={ classNames }>
         <span className="todo-list-item-label" style={ style } onClick={ this.onLabelClick }>{ label }</span>
         <button className="btn btn-outline-success btn-sm float-right" type="button"><i className="fa fa-exclamation" /></button>
         <button className="btn btn-outline-danger btn-sm float-right" type="button"><i className="fa fa-trash-o" /></button>
