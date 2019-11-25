2a6cfa605db8e959a49cb4cac651c4ac53f15765
orangepuer <rkhpuer@gmail.com>
2019-11-25 17:21:15 +0300

    Add onMarkImportant event handler to TodoListItem component

--- src/components/todo-list-item/todo-list-item.js
+++ src/components/todo-list-item/todo-list-item.js
@@ -3,7 +3,8 @@ import './todo-list-item.css'
 
 export default class TodoListItem extends Component {
   state = {
-    done: false
+    done: false,
+    important: false
   };
 
   onLabelClick = () => {
@@ -13,24 +14,29 @@ export default class TodoListItem extends Component {
     });
   };
 
+  onMarkImportant = () => {
+    this.setState({
+      important: true
+    });
+  };
+
   render() {
-    const { label, important = false } = this.props;
-    const { done } = this.state;
+    const { label } = this.props;
+    const { done, important } = this.state;
     let classNames = "todo-list-item"
 
     if(done) {
       classNames += " done"
     }
 
-    const style = {
-      color: important ? 'tomato' : 'black',
-      fontWeight: important ? 'bold' : 'normal'
-    };
+    if(important) {
+      classNames += ' important';
+    }
 
     return (
       <span className={ classNames }>
-        <span className="todo-list-item-label" style={ style } onClick={ this.onLabelClick }>{ label }</span>
-        <button className="btn btn-outline-success btn-sm float-right" type="button"><i className="fa fa-exclamation" /></button>
+        <span className="todo-list-item-label" onClick={ this.onLabelClick }>{ label }</span>
+        <button className="btn btn-outline-success btn-sm float-right" type="button" onClick={ this.onMarkImportant }><i className="fa fa-exclamation" /></button>
         <button className="btn btn-outline-danger btn-sm float-right" type="button"><i className="fa fa-trash-o" /></button>
       </span>
     );
