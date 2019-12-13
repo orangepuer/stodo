1eb709cb467a8a0e4884172f3a5355cb39bbef9d
orangepuer <rkhpuer@gmail.com>
2019-12-13 15:54:11 +0300

    Implement the ability to add your own tasks

--- src/components/item-add-form/item-add-form.js
+++ src/components/item-add-form/item-add-form.js
@@ -2,13 +2,28 @@ import React, { Component } from 'react';
 import './item-add-form.css';
 
 export default class ItemAddForm extends Component {
+  state = {
+    label: ''
+  };
+
+  onLabelChange = (e) => {
+    this.setState({
+      label: e.target.value
+    })
+  };
+
+  onSubmit = (e) => {
+    e.preventDefault();
+    const { onItemAdded } = this.props;
+    onItemAdded(this.state.label);
+  };
 
   render() {
-    const { onItemAdded } = this.props;
     return (
-      <div className="item-add-form">
-        <button className={ 'btn btn-outline-secondary' } onClick={ () => onItemAdded('newAwesomeTask') }>Add Item</button>
-      </div>
+      <form className="item-add-form" onSubmit={ this.onSubmit }>
+        <input type="text" placeholder="What needs to be done" className="form-control" onChange={ this.onLabelChange }/>
+        <button className={ 'btn btn-outline-secondary' }>Add Item</button>
+      </form>
     )
   }
 }
